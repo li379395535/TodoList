1ada9471b8fded66ec6915a2a8e6e54b706ad94c
Lee <379395535@QQ.com>
2019-07-03 21:48:48 +0800

    Config Project

---
+++ src/models/user.ts
@@ -0,0 +1,48 @@
+import { Effect } from 'dva';
+import { Reducer } from 'redux';
+interface CurrentUser{
+    Avantar?:string;
+    Name:string;
+    Account:string;
+}
+
+interface UserModelState{
+    currentUser?:CurrentUser;
+}
+
+interface UserModelType{
+    namespace:'user';
+    state:UserModelState;
+    effects:{
+        fetch:Effect;
+    };
+    reducer:{
+        saveCurrentUser:Reducer<UserModelState>;        
+    }
+}
+
+const UserModel:UserModelType = {
+    namespace:'user',
+    state:{
+        currentUser:{},
+    },
+    effects:{
+        *fetch(_,{call,put}){
+            yield put({
+                type:'saveCurrentUser',                
+            })
+        },
+
+    },
+    reducer:{
+        saveCurrentUser(state,action){
+            return{
+                ...state,
+                currentUser:action.payload||{},
+            }
+        }
+    }
+
+}
+
+export default UserModel;
